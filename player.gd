4fad9f89310cfbec0fb70e15f7a962928f5debe9
brageaasen <brageaasen@hotmail.com>
2023-09-18 11:35:00 +0200

    Init files

---
+++ player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("down"):
+		velocity.y += speed
+	if Input.is_action_pressed("up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	
+	move_and_slide()
+	look_at(get_global_mouse_position())
