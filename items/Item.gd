50dcc17262bb3125579e16cb1e0dceae41f062fa
Eivind Sulen <eivindsulen2002@gmail.com>
2023-10-06 14:36:10 +0200

    Created generic item and itemdrop scenes. Wrote some TODOs in their scripts with their purposes. Added placeholder generic item texture and mg ammo texture

---
+++ items/Item.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# TODO:
+# Denne noden skal representere en item med en texture. 
+# Den skal kunne ligge i et inventory eller instansieres gjennom ItemDrop for å ligge på bakken.
+# Det er en abstrakt klasse som skal implementeres i en egen scene. For eksempel ammo-item eller senere powerups.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ items/ItemDrop.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# TODO:
+# Denne noden skal ha en Item.tscn instansiert, og gi den en plassering på kartet, og pickup sone.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
