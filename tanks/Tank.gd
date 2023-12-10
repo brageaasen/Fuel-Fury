6179c5282fb474612298b89363b2e426a20e0004
brageaasen <brageaasen@hotmail.com>
2023-12-10 19:39:58 +0100

    Added recoil to both weapons. Recoil varies between 5 and -5 degrees if player shoots for long enough. Recoil goes down automatically when stopped shooting.

--- tanks/Tank.gd
+++ tanks/Tank.gd
@@ -11,6 +11,9 @@ signal dead
 @export var machine_gun_cooldown : float
 @export var health : int
 
+@export var max_recoil : float = 10.0
+var current_recoil = 0.0
+
 var can_shoot = true
 var alive = true
 
@@ -26,13 +29,23 @@ func shoot(bullet, turret):
 	var bullet_scene_path = bullet.get_path().get_file()
 	
 	if can_shoot:
-		can_shoot = false
+		can_shoot = false	
 		
 		# Check what type of bullet was shot
 		if bullet_scene_path.match("*MachineGunBullet*"): $MachineGunTimer.start()
 		else: $GunTimer.start()
 		var dir = Vector2(1, 0).rotated($Weapon.global_rotation)
-		emit_signal("shootSignal", bullet, $Weapon/Muzzle.global_position, dir)
+		var recoil_degree_max = current_recoil * 0.5
+		var recoil_radians_actual = deg_to_rad(randf_range(-recoil_degree_max, recoil_degree_max))
+		var actual_bullet_direction = dir.rotated(recoil_radians_actual)	
+		
+		
+		# TODO: Add recoil
+		var recoil_increment = max_recoil * 0.1
+		current_recoil = clamp(current_recoil + recoil_increment, 0.0, max_recoil)
+		
+		
+		emit_signal("shootSignal", bullet, $Weapon/Muzzle.global_position, actual_bullet_direction)
 
 func take_damage(damage):
 	health -= damage
@@ -46,6 +59,11 @@ func _physics_process(delta):
 		return
 	control(delta)
 	move_and_slide()
+	
+	var recoil_increment = max_recoil * 0.05
+	if not Input.is_action_pressed("left_click") or Input.is_action_pressed("right_click"):
+		current_recoil = clamp(current_recoil - recoil_increment, 0.0, max_recoil)
+	print(current_recoil)
 
 func _on_GunTimer_timeout():
 	can_shoot = true
