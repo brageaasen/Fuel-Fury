f3f60daa9fa112629c9b27cfba2e2e458ddc7f46
brageaasen <brageaasen@hotmail.com>
2023-12-19 13:34:36 +0100

    safe commit. Worked more on implementing ability choice UI.

--- scripts/HUD/HUD.gd
+++ scripts/HUD/HUD.gd
@@ -3,13 +3,15 @@ extends Control
 var player # Reference to the player node
 var possible_abilities
 var current_abilities
-@onready var ability_choice_1 = $AbilityChoice1
 
 func _ready():
 	player = get_node("/root/MainScene/Player")
 	possible_abilities = {"machine_gun" : player.load_ability("machine_gun")}
 	current_abilities = player.abilities
-	print(player)
+	# Connect button signals
+	$AbilityContainer/AbilityChoice0/Button.connect("pressed", _on_ability_choice_0_pressed)
+	$AbilityContainer/AbilityChoice1/Button.connect("pressed", _on_ability_choice_1_pressed)
+	$AbilityContainer/AbilityChoice2/Button.connect("pressed", _on_ability_choice_2_pressed)
 
 func update_healthbar(value):
 	var tween = create_tween()
@@ -30,7 +32,7 @@ func _on_player_ammo_updated(bullet, ammo_count):
 
 
 
-# Abilities
+## Abilities
 
 var abilities_to_display = []
 
@@ -42,41 +44,80 @@ func _on_player_leveled_up():
 			var random_ability = possible_abilities.keys()[randi() % possible_abilities.size()]
 			# Check if player already has the ability
 			var ability = possible_abilities[random_ability]
-			if not current_abilities.has(ability):
+			if not current_abilities.has(ability) and ability not in abilities_to_display:
 				abilities_to_display.append(ability)
 				count += 1
-
+			
 		# Display abilities
-		$AbilityChoice1.visible = true
-		$AbilityChoice1/Button/Icon/Name.text = abilities_to_display[0].title
-		$AbilityChoice1/Button/Icon.texture = abilities_to_display[0].image
-		$AbilityChoice1/Button/InfoBox/Info.text = abilities_to_display[0].info
-		# TODO: Do the same for rest of the buttons
+		for i in range(0, 3): # Loop from 0 to 2 inclusive
+			# Access each AbilityChoice node dynamically
+			var ability_choice = get_node("AbilityContainer").get_node("AbilityChoice" + str(i))
+			if abilities_to_display[i] != null:
+				# Set visibility to true for the current AbilityChoice
+				ability_choice.visible = true
+				
+				# Set properties based on the index of ability
+				ability_choice.get_node("Button/Icon/Name").text = abilities_to_display[i].title
+				ability_choice.get_node("Button/Icon").texture = abilities_to_display[i].image
 
 func ability_chosen(ability_number):
 	# Add ability to players ability list
 	player.abilities[abilities_to_display[ability_number]._name] = abilities_to_display[ability_number]
 	# Remove ability from possible abilities
 	possible_abilities.erase(abilities_to_display[ability_number]._name)
+	
 	# Make all buttons non visble to the player
-	$AbilityChoice1.visible = false
-	$AbilityChoice1/Button/Icon/Name.text = "null"
-	#$AbilityChoice1/Button/Icon.texture = null
-	$AbilityChoice1/Button/InfoBox/Info.text = "null"
+	for i in range(0, 3): # Loop from 0 to 2 inclusive
+		# Access each AbilityChoice node dynamically
+		var ability_choice = get_node("AbilityContainer").get_node("AbilityChoice" + str(i))
+		if abilities_to_display[i] != null:
+			# Set visibility to false for the current AbilityChoice
+			ability_choice.visible = false
+			
+			# Set properties based on the index of ability
+			ability_choice.get_node("Button/Icon/Name").text = ""
+			ability_choice.get_node("Button/Icon").texture = null
 	# Reset new abilities list
 	abilities_to_display = []
 
-func ability_hover():
-	pass
+func ability_hover(ability):
+	# Enable infobox
+	$InfoContainer/InfoBox.visible = true
+	# Set the label's text
+	$InfoContainer/InfoBox/MarginContainer/Info.text = abilities_to_display[ability].info
 
-func _on_ability_choice_1_pressed():
+func ability_non_hover():
+	# Disable infobox
+	$InfoContainer/InfoBox.visible = false
+	# Reset the label's text
+	$InfoContainer/InfoBox/MarginContainer/Info.text = ""
+
+# Signals for ability button 0
+func _on_ability_choice_0_pressed():
 	ability_chosen(0)
 
+func _on_ability_choice_0_mouse_entered():
+	ability_hover(0)
+
+func _on_ability_choice_0_mouse_exited():
+	ability_non_hover()
+
+# Signals for ability button 1
+func _on_ability_choice_1_pressed():
+	ability_chosen(1)
 
 func _on_ability_choice_1_mouse_entered():
-	print("active")
-	ability_hover()
-
+	ability_hover(1)
 
 func _on_ability_choice_1_mouse_exited():
-	print("not active")
+	ability_non_hover()
+
+# Signals for ability button 2
+func _on_ability_choice_2_pressed():
+	ability_chosen(2)
+
+func _on_ability_choice_2_mouse_entered():
+	ability_hover(2)
+
+func _on_ability_choice_2_mouse_exited():
+	ability_non_hover()
